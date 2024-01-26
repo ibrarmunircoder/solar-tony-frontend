c4e113c7a033849ea01bcb62f88571b2a54d8dff
ibrarmunircoder <ibrarmunir009@gmail.com>
2024-01-27 01:36:17 +0500

    Added search filters

--- src/pages/home/hooks/useSolarProducts.tsx
+++ src/pages/home/hooks/useSolarProducts.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { solarProductsService } from "shared/services";
 import { GridColDef } from "@mui/x-data-grid";
 import { SolarPanelProduct } from "shared/types";
@@ -24,13 +24,7 @@ const columns: GridColDef[] = [
     width: 142,
     filterable: false,
   },
-  {
-    headerName: "Availability",
-    field: "Availability",
-    disableColumnMenu: true,
-    width: 120,
-    filterable: false,
-  },
+
   {
     headerName: "Condition",
     field: "Condition",
@@ -46,6 +40,9 @@ const columns: GridColDef[] = [
     disableColumnMenu: true,
     width: 80,
     filterable: false,
+    renderCell(params) {
+      return params.row.Color === "N/A" ? null : params.row.Color;
+    },
   },
 
   {
@@ -85,53 +82,32 @@ const columns: GridColDef[] = [
       );
     },
   },
-
-  //   {
-  //     headerName: "Height",
-  //     disableColumnMenu: true,
-  //     field: "Height",
-  //     width: 200,
-  //     filterable: false,
-  //     sortable: false,
-  //     renderCell(params) {
-  //       return `${params?.row?.Height} ${params?.row?.HeightUnit}`;
-  //     },
-  //   },
-  //   {
-  //     headerName: "Width",
-  //     disableColumnMenu: true,
-  //     field: "Width",
-  //     width: 200,
-  //     filterable: false,
-  //     sortable: false,
-  //     renderCell(params) {
-  //       return `${params?.row?.Width} ${params?.row?.WidthLength}`;
-  //     },
-  //   },
-  //   {
-  //     headerName: "Length",
-  //     disableColumnMenu: true,
-  //     field: "Length",
-  //     width: 200,
-  //     filterable: false,
-  //     sortable: false,
-  //     renderCell(params) {
-  //       return `${params?.row?.Length} ${params?.row?.LengthUnit}`;
-  //     },
-  //   },
 ];
 
 const useSolarProducts = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [filter, setFilter] = useState({
+    min: "",
+    max: "",
+    brand: "",
+    size: "",
+  });
+  const newConditionCheckboxRef = useRef<HTMLInputElement>(null);
+  const usedConditionCheckboxRef = useRef<HTMLInputElement>(null);
+  const dataRef = useRef<SolarPanelProduct[]>([]);
+
   const [pageInfo, setPageInfo] = useState<IPageInfo>({
     rows: [],
     rowCount: 0,
   });
 
   useEffect(() => {
+    setIsLoading(true);
     solarProductsService
       .getSolarProducts()
       .then((data) => {
         const products = data as SolarPanelProduct[];
+        dataRef.current = products;
         setPageInfo({
           rows: products,
           rowCount: products.length,
@@ -139,12 +115,133 @@ const useSolarProducts = () => {
       })
       .catch((error) => {
         console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, []);
 
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFilter((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handlePriceSearch = () => {
+    if (filter.min && filter.max) {
+      const newProducts = dataRef.current.filter((row) => {
+        return (
+          parseFloat(row.Price) >= parseFloat(filter.min) &&
+          parseFloat(row.Price) <= parseFloat(filter.max)
+        );
+      });
+      setPageInfo(() => ({
+        rowCount: newProducts.length,
+        rows: newProducts,
+      }));
+      return;
+    }
+    if (filter.min) {
+      const newProducts = dataRef.current.filter(
+        (row) => parseFloat(row.Price) === parseFloat(filter.min),
+      );
+      setPageInfo(() => ({
+        rowCount: newProducts.length,
+        rows: newProducts,
+      }));
+      return;
+    }
+    if (filter.max) {
+      const newProducts = dataRef.current.filter(
+        (row) => parseFloat(row.Price) === parseFloat(filter.max),
+      );
+      setPageInfo(() => ({
+        rowCount: newProducts.length,
+        rows: newProducts,
+      }));
+      return;
+    }
+  };
+
+  const handleBrandSearch = () => {
+    if (filter.brand) {
+      const newProducts = dataRef.current.filter((row) =>
+        row.Brand.toLowerCase().startsWith(filter.brand),
+      );
+      setPageInfo(() => ({
+        rowCount: newProducts.length,
+        rows: newProducts,
+      }));
+    }
+  };
+  const handleSizeSearch = () => {
+    if (filter.size) {
+      const newProducts = dataRef.current.filter((row) =>
+        row.Size.includes(filter.size),
+      );
+      setPageInfo(() => ({
+        rowCount: newProducts.length,
+        rows: newProducts,
+      }));
+    }
+  };
+
+  const handleConditionSearch = function (
+    event: React.ChangeEvent<HTMLFieldSetElement>,
+  ) {
+    event.stopPropagation();
+    const isNewChecked = newConditionCheckboxRef.current?.checked;
+    const isUsedChecked = usedConditionCheckboxRef.current?.checked;
+    if (isNewChecked && isUsedChecked) {
+      const newProducts = dataRef.current.filter((row) => {
+        return (
+          row.Condition.toLowerCase() === "new" ||
+          row.Condition.toLowerCase() === "used"
+        );
+      });
+      setPageInfo(() => ({
+        rowCount: newProducts.length,
+        rows: newProducts,
+      }));
+    } else if (isNewChecked) {
+      const newProducts = dataRef.current.filter((row) => {
+        return row.Condition.toLowerCase() === "new";
+      });
+      setPageInfo(() => ({
+        rowCount: newProducts.length,
+        rows: newProducts,
+      }));
+    } else if (isUsedChecked) {
+      const newProducts = dataRef.current.filter((row) => {
+        return row.Condition.toLowerCase() === "used";
+      });
+      setPageInfo(() => ({
+        rowCount: newProducts.length,
+        rows: newProducts,
+      }));
+    } else {
+      setPageInfo(() => ({
+        rowCount: 0,
+        rows: [],
+      }));
+    }
+  };
+
   return {
     ...pageInfo,
+    isLoading,
     columns,
+    newConditionCheckboxRef,
+    usedConditionCheckboxRef,
+    handleOnChange,
+    handlePriceSearch,
+    handleBrandSearch,
+    handleSizeSearch,
+    handleConditionSearch,
   };
 };
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,31 +1,150 @@
 import { useSolarProducts } from "./hooks";
 import { StyledDataGrid } from "pages/home/home.styles";
+import TextField from "@mui/material/TextField";
+import { InputAdornment } from "@mui/material";
 
 const Home = () => {
-  const { columns, rowCount, rows } = useSolarProducts();
+  const {
+    columns,
+    rowCount,
+    isLoading,
+    rows,
+    newConditionCheckboxRef,
+    usedConditionCheckboxRef,
+    handleOnChange,
+    handlePriceSearch,
+    handleBrandSearch,
+    handleSizeSearch,
+    handleConditionSearch,
+  } = useSolarProducts();
   return (
     <div className="flex p-3 gap-3">
       <div>
-        <fieldset className="condition">
+        <fieldset className="condition" onChange={handleConditionSearch}>
           <legend>Condition</legend>
           <label>
-            <input type="checkbox" data-condition="new" checked />
+            <input
+              type="checkbox"
+              data-condition="new"
+              defaultChecked
+              ref={newConditionCheckboxRef}
+            />
             New
           </label>
           <label>
-            <input type="checkbox" data-condition="used" checked />
+            <input
+              type="checkbox"
+              data-condition="used"
+              defaultChecked
+              ref={usedConditionCheckboxRef}
+            />
             Used
           </label>
         </fieldset>
+        <fieldset className="price">
+          <legend>Price</legend>
+          <span className="block">Under $25</span>
+          <span className="block">$25 to $50</span>
+          <span className="block">$50 to $100</span>
+          <span className="block">$100 to $200</span>
+          <span className="block">$200 & Above</span>
+          <div className="flex gap-2 mt-2">
+            <TextField
+              sx={{
+                width: "75px",
+                "&  fieldset": {
+                  width: "75px",
+                },
+              }}
+              name="min"
+              onChange={handleOnChange}
+              placeholder="Min"
+              size="small"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              sx={{
+                width: "75px",
+                "&  fieldset": {
+                  width: "75px",
+                },
+              }}
+              name="max"
+              onChange={handleOnChange}
+              placeholder="Max"
+              size="small"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+              }}
+            />
+          </div>
+          <button
+            onClick={handlePriceSearch}
+            className="w-full border border-black rounded-md mt-2 border-solid px-4 py-2"
+          >
+            Search
+          </button>
+        </fieldset>
+        <fieldset>
+          <legend>Brand</legend>
+          <TextField
+            sx={{
+              width: "130px",
+              "&  fieldset": {
+                width: "130px",
+              },
+            }}
+            name="brand"
+            onChange={handleOnChange}
+            placeholder="Brand"
+            size="small"
+          />
+          <button
+            onClick={handleBrandSearch}
+            className="w-full border border-black rounded-md mt-2 border-solid px-4 py-2"
+          >
+            Search
+          </button>
+        </fieldset>
+        <fieldset>
+          <legend>Size</legend>
+          <TextField
+            sx={{
+              width: "130px",
+              "&  fieldset": {
+                width: "130px",
+              },
+            }}
+            placeholder="Size"
+            name="size"
+            onChange={handleOnChange}
+            size="small"
+          />
+          <button
+            onClick={handleSizeSearch}
+            className="w-full border border-black rounded-md mt-2 border-solid px-4 py-2"
+          >
+            Search
+          </button>
+        </fieldset>
       </div>
-      <StyledDataGrid
-        getRowId={(row) => row._id}
-        columns={columns}
-        rows={rows}
-        rowCount={rowCount}
-        getRowHeight={() => "auto"}
-        hideFooterPagination={true}
-      />
+      <div style={{ height: 500, width: "100%" }}>
+        <StyledDataGrid
+          loading={isLoading}
+          getRowId={(row) => row._id}
+          columns={columns}
+          rows={rows}
+          rowCount={rowCount}
+          getRowHeight={() => "auto"}
+          hideFooterPagination={true}
+        />
+      </div>
     </div>
   );
 };
